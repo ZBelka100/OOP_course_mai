fc71da81487a217b12e003482e06d76d980398e7
ZBelka100 <andreyzabelkin333@gmail.com>
2023-11-09 19:54:47 +0300

    Удалил мусор

--- test_lab4/lab4/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andrey/Desktop/cpp_memes/oop/OOP_course_mai/test_lab4/lab4
-# Build directory: /home/andrey/Desktop/cpp_memes/oop/OOP_course_mai/test_lab4/lab4/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(BASIC "/home/andrey/Desktop/cpp_memes/oop/OOP_course_mai/test_lab4/lab4/build/tests3")
-set_tests_properties(BASIC PROPERTIES  _BACKTRACE_TRIPLES "/home/andrey/Desktop/cpp_memes/oop/OOP_course_mai/test_lab4/lab4/CMakeLists.txt;16;add_test;/home/andrey/Desktop/cpp_memes/oop/OOP_course_mai/test_lab4/lab4/CMakeLists.txt;0;")
